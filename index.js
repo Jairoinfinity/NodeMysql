da6a4e3a6603a4abfa52dca85b071e05eb33e135
Jairo <jairobernaldev@gmail.com>
2019-03-12 14:22:01 +0100

    ejs

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 var express = require("express");
 var mySql = require("mysql");
-
 var app = express();
 
-app.listen(8081, function(){
-    console.log("Servidor corriendo en http://localhost:8081");
+app.set('views', 'html');
+app.set('view engine', 'ejs');
+
+var conexion = mySql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "nodemysql"
 });
 
-app.use("/js", function(request, response){
-    response.sendFile(__dirname + "/js/main.js");
-});
+app.listen(8081);
 
-app.use("/", function(request, response){
-    response.sendFile(__dirname +  "/index.html");
-});
+app.use("/:ciudad", function (req, res) {
+    var sql = "SELECT * FROM temp WHERE ciudad = '" + req.params.ciudad + "'"
+    console.log(sql)
+    conexion.query(sql, function (err, datos) {
+        if (err) {
+            console.log("Error en la consulta");
+        } else {
+            let variables = {
+                Ciudad: req.params.ciudad,
+                temp: datos[0].temp,
+                tempMax: datos[0].tempMax,
+                tempMin: datos[0].tempMin
+            }
 
+            res.render("index", variables);
+        }
+    });
+}
+);
